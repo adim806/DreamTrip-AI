89ae8bc612f99557e74062353f2dab289fd86e0a
Adim806 <adim806@gmail.com>
2024-11-02 20:03:55 +0200

    added view trip details (comp folder+index dynmic )

---
+++ client/src/routes/view-trip/compo/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+
+  
+  return (
+    <div className="my-16">
+        <div>
+            <h2 className="text-center text-gray-400">Created by Meller boy...</h2>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
