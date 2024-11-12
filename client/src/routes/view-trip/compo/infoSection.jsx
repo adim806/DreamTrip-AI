3dd481d40bcf748b87b8b5a0575ce83494765d4b
Adim806 <adim806@gmail.com>
2024-11-12 23:08:10 +0200

    add info section but need to sync usestate hooks

--- client/src/routes/view-trip/compo/infoSection.jsx
+++ client/src/routes/view-trip/compo/infoSection.jsx
@@ -5,21 +5,21 @@ import { useEffect, useState } from 'react';
 import { IoIosSend } from "react-icons/io";
 
 
-const InfoSection = ({trip}) => {
+const InfoSection = ({data}) => {
   console.log("IN infoSection FUNCTION");
 
   const [photo_url, setPhotoUrl] = useState();
   useEffect(()=>{
-    trip&&getPlacePhoto();
-  },[trip])
+    data&&getPlacePhoto();
+  },[data])
 
 
   const getPlacePhoto=async()=>{
     const data={
-      textQuery:trip?.userSelection?.vacation_location
+      textQuery:'Thailand'
     }
     const result= await getPlaceDetails(data).then(resp=>{
-      //console.log(resp.data.places[0].photos[3].name);
+      console.log(resp.data.places[0].photos[3].name);
       const photo_url=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name);
       setPhotoUrl(photo_url);
     });
@@ -34,11 +34,11 @@ const InfoSection = ({trip}) => {
       <div className="flex justify-between items-center">
         <div className='my-5 flex flex-col gap-2'>
           <h2 className='font-bold text-2xl'>
-            {trip?.userSelection?.vacation_location}</h2>
+            Thiland</h2>
           <div className="flex gap-5">
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>📅{trip?.userSelection?.duration} Day</h2>
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>💰{trip?.userSelection?.constraints?.budget} Budget</h2>
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>🥂No. of travlers: {trip?.userSelection?.constraints?.travel_type}</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>📅5 Day</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>💰 Budget</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-md md:text-md'>🥂No. of travlers:</h2>
 
           </div>
         </div>
