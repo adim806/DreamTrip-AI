f9d9a717f5e4c7491bff76e1a90c9eecb749a83f
liel26 <118679526+liel26@users.noreply.github.com>
2025-01-20 18:42:06 +0200

    containers fixed

--- client/src/routes/createTrip/ViewTripData.jsx
+++ client/src/routes/createTrip/ViewTripData.jsx
@@ -1,36 +1,33 @@
-
-import './createTrip.css';
-import ViewMap from '../view-trip/compo/ViewMap';
-import { TripContext, TripProvider } from '@/components/tripcontext/TripProvider';
-import { useContext, useEffect } from 'react';
+import ViewMap from "../view-trip/compo/ViewMap";
+import {
+  TripContext,
+  TripProvider,
+} from "@/components/tripcontext/TripProvider";
+import { useContext, useEffect } from "react";
+import LeftContainer from "../view-trip/compo/LeftContainer";
 
 const ViewTripData = () => {
   console.log("IN ViewTripData FUNCTION");
   const { tripDetails, setTripDetails } = useContext(TripContext);
-
 
   useEffect(() => {
     console.log("Trip details updated:", tripDetails);
     // עדכונים נוספים בהתאם לטריגר
   }, [tripDetails]);
   return (
-    <div >
-      <div >
-        <ViewMap trip={tripDetails}/>
+    <div
+      style={{
+        height: "90vh", // Full viewport height
+        display: "flex", // Flexbox container for side-by-side layout
+        margin: "0", // Ensure no margins cause overflow
+        padding: "0", // Remove padding
+      }}
+    >
+      {/* Left Container */}
+      <LeftContainer />
 
-      </div>
-
-      {/* Information section <InfoSection/>*/}
-      
-
-      {/* hotel section <Hotels trip={trip}/>*/}
-      
-
-      {/* daily plan <PlacesToVisit trip={trip}/>*/}
-      
-
-      {/* footer <Footer/>*/}
-      
+      {/* Right Container */}
+      <ViewMap trip={tripDetails} />
     </div>
   );
 };
---
+++ client/src/routes/view-trip/compo/LeftContainer.jsx
@@ -0,0 +1,16 @@
+const LeftContainer = ({}) => {
+  return (
+    <div
+      style={{
+        flex: 1, // Take up half the width
+        height: "100%", // Full height of parent
+        boxSizing: "border-box", // Ensure borders are included in dimensions
+        background: "black",
+      }}
+    >
+      <p>hey hey hey</p>
+    </div>
+  );
+};
+
+export default LeftContainer;
