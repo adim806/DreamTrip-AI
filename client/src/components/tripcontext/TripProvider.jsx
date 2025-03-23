7b91e7daa35600a6a6cb7f4f9797bfb7850a8415
Adim806 <adim806@gmail.com>
2025-03-23 16:37:20 +0200

    Hotel data on view map comp

--- client/src/components/tripcontext/TripProvider.jsx
+++ client/src/components/tripcontext/TripProvider.jsx
@@ -6,29 +6,42 @@ export const TripContext = createContext();
 export function TripProvider({ children }) {
   const [tripDetails, setTripDetails] = useState(null);
   const [allTripData, setallTripData] = useState(null);
-  // ✅ שמירת נתונים לכל קטגוריה
+  // נתונים לכל קטגוריה
   const [hotelsData, setHotelsData] = useState(null);
   const [restaurantsData, setRestaurantsData] = useState(null);
   const [attractionsData, setAttractionsData] = useState(null);
   const [customRouteData, setCustomRouteData] = useState(null);
 
-    // ✅ ניהול השכבה הפעילה במפה ותפריט ברירת מחדל
-    const [activeLayer, setActiveLayer] = useState(null);
-    const [defaultTab, setDefaultTab] = useState("generalInfo"); // ✅ ברירת מחדל ל"תיאור כללי"
+  // ניהול השכבה הפעילה במפה ותפריט ברירת מחדל
+  const [activeLayer, setActiveLayer] = useState(null);
+  const [defaultTab, setDefaultTab] = useState("generalInfo"); // ברירת מחדל ל"תיאור כללי"
+
+  // משתנה לניהול המלון הנבחר
+  const [selectedHotel, setSelectedHotel] = useState(null);
 
   return (
-    <TripContext.Provider value={{ 
-      tripDetails, setTripDetails, 
-      allTripData, setallTripData, 
-      
-      hotelsData, setHotelsData,
-      restaurantsData, setRestaurantsData,
-      attractionsData, setAttractionsData,
-      customRouteData, setCustomRouteData,
-      
-      activeLayer, setActiveLayer,
-      defaultTab, setDefaultTab // ✅ ניהול הלשונית הראשונה
-    }}>
+    <TripContext.Provider
+      value={{
+        tripDetails,
+        setTripDetails,
+        allTripData,
+        setallTripData,
+        hotelsData,
+        setHotelsData,
+        restaurantsData,
+        setRestaurantsData,
+        attractionsData,
+        setAttractionsData,
+        customRouteData,
+        setCustomRouteData,
+        activeLayer,
+        setActiveLayer,
+        defaultTab,
+        setDefaultTab,
+        selectedHotel,
+        setSelectedHotel,
+      }}
+    >
       {children}
     </TripContext.Provider>
   );
